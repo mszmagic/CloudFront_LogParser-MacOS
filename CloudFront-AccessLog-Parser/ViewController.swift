0f57663ce5012fa9c37bd4fc9360257d9740e0d0
Shunzhe <git@mszmagic.com>
2021-03-08 13:01:43 -0500

    Initial Commit

---
+++ CloudFront-AccessLog-Parser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CloudFront-AccessLog-Parser
+//
+//  Created by Shunzhe Ma on R 3/03/08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
